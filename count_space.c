910bf982a6272181726d8c606be603fe9b90d4b6
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-04-06 21:14:27 +0530

    Create count_space.c

---
+++ count_space.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char input[50],space=0,i;
+  gets(input);
+  for(i=0;input[i] !='\0' ;i++)
+  {
+  	if(input[i] == ' ')
+  	{
+  		space++;
+  	}
+  	
+  }
+   printf("%d",word);
+   return 0;
+ }
