6b152887dc3c0cf33a9e072bcd5f3685d554bff9
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-04-07 13:39:47 +0530

    Update alphanum.c

--- alphanum.c
+++ alphanum.c
@@ -1,9 +1,22 @@
-#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
 int main()
 {
   char str[20];
-  scanf("%s",&str);\
-  if(isalnum(str))
+  gets(str);
+  int i,count_alp=0,count_num=0;
+  for(i=0;str[i] != '\0';i++)
+  {
+	if((str[i] >= 'A') && (str[i] <= 'z') )
+	{
+		count_alp=1;
+	}
+	else if((str[i] >= '0') && (str[i] <= '9') )
+	{
+		count_num=1;
+	}
+  }
+  if((count_alp == 1) && (count_num == 1))
   {
     printf("yes");
   }
