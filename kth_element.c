0cdb79ea59c9bc8848b2b4129c5f4853fcfd2d7a
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-04-20 23:01:36 +0530

    Create kth_element.c

---
+++ kth_element.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int N,K,i;
+  scanf("%d %d",&N,&K);
+  int arr[N];
+  for(i=0;i<N;i++)
+  {
+    scanf("%d",&arr[i]);
+  }
+  for(i=0;i<N;i++)
+  {
+    printf("%d",arr[K-1]);
+  }
+  
+  return 0;
+}
